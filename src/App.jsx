b1f791ae67e80791e25085c0a9ae520ef4ce7456
Sanjay-250502 <sanjayramachandran2002@gmail.com>
2023-09-26 22:34:07 +0530

    price card updated

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import Card from "./Card.jsx"
+import {useState} from 'react' 
+
+function App() {
+  let [count,setCount] = useState(0)   //HOOKS- it is a function.
+
+  const data =[
+    {
+      plan:"FREE",
+      price:"0",
+      user:"Single User",
+      userEnabler: true,
+      storage:"5GB Storage",
+      storageEnabler: true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess:"Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: false,
+      support: "Dedicated Phone Support",
+      supportEnabler: false,
+      subDomain: "Free Subdomain",
+      subDomainEnabler: false,
+      reports: "Monthly Status Reports",
+      reportsEnabler: false
+    },
+    {
+      plan:"PLUS",
+      price:"9",
+      user:"5 Users",
+      userEnabler: true,
+      storage:"50GB Storage",
+      storageEnabler: true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess:"Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: true,
+      support: "Dedicated Phone Support",
+      supportEnabler: true,
+      subDomain: "Free Subdomain",
+      subDomainEnabler: true,
+      reports: "Monthly Status Reports",
+      reportsEnabler: false
+    },
+    {
+      plan:"PRO",
+      price:"49",
+      user:"Unlimited User",
+      userEnabler: true,
+      storage:"150GB Storage",
+      storageEnabler: true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess:"Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: true,
+      support: "Dedicated Phone Support",
+      supportEnabler: true,
+      subDomain: "Unlimited Free Subdomain",
+      subDomainEnabler: true,
+      reports: "Monthly Status Reports",
+      reportsEnabler: true
+    }
+  ]
+  
+
+  return (
+    <>
+      <section className="pricing py-5">
+  <div className="container">
+    <div className="row">
+
+    {
+      data.map((e,i)=>{
+        return <Card data = {e} key = {i} />
+        
+        {/* sending data to card components via props */}
+        
+      })
+    }
+
+    </div>
+    <div>
+      <button className="btn btn-danger"
+      onClick={()=>{
+        setCount(++count)
+        console.log(count)
+      }}
+      >Click Me, I was already clicked {count} times
+        </button>
+    </div>
+  </div>
+</section>
+    </>
+  )
+}
+
+export default App
